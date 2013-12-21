8a1d211e14d235eb90afa009f80b2a92d44be1a2
yui-knk <spiketeika@gmail.com>
2013-12-21 11:41:48 +0900

    Remove require 'bond'. Now gem Bond requires 'rb-readline', this undefines Readline constant. When gem 'bond' fix this bug, we will require 'bond' again.

--- lib/pry.rb
+++ lib/pry.rb
@@ -223,11 +223,6 @@ if Pry::Helpers::BaseHelpers.windows? && !Pry::Helpers::BaseHelpers.windows_ansi
   end
 end
 
-begin
-  require 'bond'
-rescue LoadError
-end
-
 require 'pry/version'
 require 'pry/repl'
 require 'pry/rbx_path'
